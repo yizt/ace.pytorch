a9ffa44b43cff47846b88e4e086d27e91cd5d9a0
yizt <csuyzt@163.com>
2020-07-03 09:11:14 +0800

    Synth90k数据加载

--- dataset.py
+++ dataset.py
@@ -5,6 +5,11 @@
  @Author  : yizuotian
  @Description    :
 """
+import argparse
+import codecs
+import os
+import sys
+
 import numpy as np
 import torch.utils.data as data
 from PIL import Image
@@ -64,3 +69,63 @@ class BaseDataset(data.Dataset):
         return {'image': img,
                 'target': label,
                 'gt': gt}
+
+
+class Synth90Dataset(BaseDataset):
+
+    def __init__(self, synth_root, alpha=None, transforms=None,
+                 target_transforms=None, mode='train', **kwargs):
+        super(Synth90Dataset, self).__init__(alpha=alpha,
+                                             transforms=transforms,
+                                             target_transforms=target_transforms,
+                                             mode=mode,
+                                             **kwargs)
+        self.synth_root = synth_root
+        self.image_path_list = self.get_image_path_list()
+
+    def get_image_path_list(self):
+        """
+        标注文件格式如下：
+        ./3000/6/501_UNIVERSAL_82748.jpg 82748
+        ./3000/6/500_Parameters_55458.jpg 55458
+        ./3000/6/499_SKEPTICAL_71251.jpg 71251
+        :return:
+        """
+        annotation_path = os.path.join(self.synth_root, 'annotation_{}.txt'.format(self.mode))
+        with codecs.open(annotation_path, mode='r', encoding='utf-8') as f:
+            lines = f.readlines()
+        image_path_list = []
+        for line in lines:
+            img_path, _ = line.strip().split(' ')
+            img_path = os.path.join(self.synth_root, img_path)
+            image_path_list.append(img_path)
+        return image_path_list
+
+    def get_img_path(self, index):
+        return self.image_path_list[index]
+
+    def get_gt_text(self, index):
+        image_path = self.get_img_path(index)  # eg:./3000/6/501_UNIVERSAL_82748.jpg
+        text = image_path.split('_')[1]
+        return text
+
+
+if __name__ == '__main__':
+    import string
+    from torchvision.transforms import transforms
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-i', '--syn-root', type=str, default=None)
+    parser.add_argument('--alpha', type=str, default=' ' + string.digits + string.ascii_lowercase)
+    args = parser.parse_args(sys.argv[1:])
+
+    trans = transforms.Compose([
+        transforms.Resize((32, 100)),  # [h,w]
+        transforms.ToTensor(),
+        transforms.Normalize([0.5], [0.5])
+    ])
+
+    syn = Synth90Dataset(args.syn_root, args.alpha, transforms=trans)
+
+    for i in range(10):
+        print(syn[i])
