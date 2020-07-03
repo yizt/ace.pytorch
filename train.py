4c863b39989d05d7a0128e0196aed5912f250908
yizt <csuyzt@163.com>
2020-07-03 16:36:21 +0800

    fix bug

--- train.py
+++ train.py
@@ -110,7 +110,7 @@ def train(args):
     torch.backends.cudnn.benchmark = True
     # data loader
     trans = transforms.Compose([
-        transforms.Resize((args.h, args.w)),  # [h,w]
+        transforms.Resize((args.height, args.width)),  # [h,w]
         transforms.ToTensor(),
         transforms.Normalize([0.5], [0.5])
     ])
